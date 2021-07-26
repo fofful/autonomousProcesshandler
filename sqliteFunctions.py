af071c87540dea6260e079bfc04bb03ffca0fdf7
topi huttunen <topi.huttunen@tuni.fi>
2021-07-26 17:54:00 +0300

    refactoring sqlitefunctions

--- sqliteFunctions.py
+++ sqliteFunctions.py
@@ -2,10 +2,11 @@ import sqlite3
 from sqlite3 import Error
 
 def create_db_connection(db_file, create_table_cmd):
-    """ create a database connection and table if it doesn't exixt to an SQLite database """
+    '''create a database connection and table if it doesn't exist to an SQLite database'''
     try:
         apo_db = sqlite3.connect(db_file)
-        create_table(apo_db, create_table_cmd)
+        cur = apo_db.cursor()
+        cur.execute(create_table_cmd)
     except Error as err:
         print(err)
     finally:
@@ -35,13 +36,6 @@ def create_ram_db_connection():
     finally:
         if ram_db:
             ram_db.close()
-
-def create_table(apo_db, create_table_sql):
-    try:
-        cur = apo_db.cursor()
-        cur.execute(create_table_sql)
-    except Error as err:
-        print(err)
 
 def printApos():
     try:
